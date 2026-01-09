36ea2b9fe72b1f23822dcfa276b510c3c4b32e6c
Thomas Juul Dyhr <thomas@dyhr.com>
2026-01-09 11:20:01 +0100

    fix: Simplify formula to use source tarball
    - Changed from wheel to source tarball distribution
    - Removed explicit sub-dependencies (aiohttp deps)
    - Let pip handle transitive dependency resolution
    - Reduces maintenance burden and download issues

--- Formula/macversiontracker.rb
+++ Formula/macversiontracker.rb
@@ -3,8 +3,8 @@ class Macversiontracker < Formula
 
   desc "Track and manage macOS applications installed outside the App Store"
   homepage "https://github.com/docdyhr/versiontracker"
-  url "https://files.pythonhosted.org/packages/51/9b/81c4afc855b29f73682eaddd3cc92ee9618bd6f4f37b1236ef7f24c2cc6c/macversiontracker-0.8.1-py3-none-any.whl"
-  sha256 "5da267240a04decf5249de812bb0156d94bde195d362ee87ea04833cf251ff25"
+  url "https://files.pythonhosted.org/packages/a2/fc/1b24fb43e6f832e3a1b12fd1d0d38f33ecdb37faf7e6e071a86e7e7c80df/macversiontracker-0.8.1.tar.gz"
+  sha256 "079385a465ac4fed5f4952984395ed0379b6fd2d438dca35c6c7ca9e4f95dd1e"
   license "MIT"
 
   depends_on "python@3.12"
@@ -32,46 +32,6 @@ class Macversiontracker < Formula
   resource "aiohttp" do
     url "https://files.pythonhosted.org/packages/54/07/9467d3f8f424dce0b5b92cf48ab95673a52a4bf07b99a2b9f6de747385a7/aiohttp-3.11.11.tar.gz"
     sha256 "bb49c7f1e6ebf3821a42d81c494f538107610c3a705987f53068546b0e90303e"
-  end
-
-  resource "aiohappyeyeballs" do
-    url "https://files.pythonhosted.org/packages/bc/69/2f6d5a019bd02e920a3417689a89887b39ad1e350b562f9955693d900c40/aiohappyeyeballs-2.4.4.tar.gz"
-    sha256 "5fdd7d87889c63183afc18ce9271f9b0a7d32c2303e394468dd45d514a757745"
-  end
-
-  resource "aiosignal" do
-    url "https://files.pythonhosted.org/packages/ae/67/0952ed97a9793b4958e5736f6d2b346b414a2cd63e82d05bc74a1f76d0c3/aiosignal-1.3.2.tar.gz"
-    sha256 "a8c255c66fafb1e499c9351d0bf32ff2d8a0321595ebac3b93713656d2436f54"
-  end
-
-  resource "attrs" do
-    url "https://files.pythonhosted.org/packages/48/c8/6260f8ccc11f0917360fc0da435c5c9c7504e3db174d5a12a1494887b045/attrs-24.3.0.tar.gz"
-    sha256 "8f5c07333d543103541ba7be0e2ce16eeee8130cb0b3f9238ab904ce1e85baff"
-  end
-
-  resource "frozenlist" do
-    url "https://files.pythonhosted.org/packages/8f/ed/0f4cec13a93c02c47ec32d81d11c0c1efbadf4a471e3f3ce7cad366cbbd3/frozenlist-1.5.0.tar.gz"
-    sha256 "81d5af29e61b9c8348e876d442253723928dce6433e0e76cd925cd83f1b4b817"
-  end
-
-  resource "idna" do
-    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
-    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
-  end
-
-  resource "multidict" do
-    url "https://files.pythonhosted.org/packages/d6/be/504b89a5e9ca731cd47487e91c469064f8ae5af93b7259758dcfc2b9c848/multidict-6.1.0.tar.gz"
-    sha256 "22ae2ebf9b0c69d206c003e2f6a914ea33f0a932d4aa16f236afc049d9958f4a"
-  end
-
-  resource "propcache" do
-    url "https://files.pythonhosted.org/packages/20/c8/2a13f78d82211490855b2fb303b6721348d0787fdd9a12ac46d99d3acde1/propcache-0.2.1.tar.gz"
-    sha256 "3f77ce728b19cb537714499928fe800c3dda29e8d9428778fc7c186da4c09a64"
-  end
-
-  resource "yarl" do
-    url "https://files.pythonhosted.org/packages/b7/9d/4b94a8e6d2b51b599516a5cb88e5bc99b4d8d4583e468057eaa29d5f0918/yarl-1.18.3.tar.gz"
-    sha256 "ac1801c45cbf77b6c99242eeff4fffb5e4e73a800b5c4ad4fc0be5def634d2e1"
   end
 
   def install
