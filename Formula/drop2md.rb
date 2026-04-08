ee6361f827c7a3a0f5d3f9ba5ba77f1de5b3ee0c
Thomas Juul Dyhr <thomas@dyhr.com>
2026-04-08 10:11:59 +0200

    feat: Add drop2md formula v0.5.0

---
+++ Formula/drop2md.rb
@@ -0,0 +1,6 @@
+class Drop2md < Formula
+  desc "macOS document-to-markdown converter with folder watching and LLM integration"
+  homepage "https://github.com/docdyhr/drop2md"
+  license "MIT"
+  version "0.5.0"
+end
