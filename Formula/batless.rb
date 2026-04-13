759b804de80d8cafd408f7093fc50a33b5e7da79
Thomas Juul Dyhr <thomas@dyhr.com>
2026-04-13 21:59:06 +0200

    chore: update batless to v0.6.0

--- Formula/batless.rb
+++ Formula/batless.rb
@@ -1,22 +1,22 @@
 class Batless < Formula
   desc "A non-blocking, LLM-friendly code viewer inspired by bat"
   homepage "https://github.com/docdyhr/batless"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/docdyhr/batless/releases/download/v0.5.0/batless-aarch64-apple-darwin.tar.gz"
-      sha256 "b5b281bae51cb06178b614abe14d3c438e35f7a56fe70caa85aca317bb5ed3bb"
+      url "https://github.com/docdyhr/batless/releases/download/v0.6.0/batless-aarch64-apple-darwin.tar.gz"
+      sha256 "5c87e3fddc1878cef843b6b4d257b472cc0ad30495a324350ff92a774e9bee5e"
     else
-      url "https://github.com/docdyhr/batless/releases/download/v0.5.0/batless-x86_64-apple-darwin.tar.gz"
-      sha256 "81c3f2deddcae8df1e523286661ebed618cc9fdc73361b30ed55ed87573a6510"
+      url "https://github.com/docdyhr/batless/releases/download/v0.6.0/batless-x86_64-apple-darwin.tar.gz"
+      sha256 "4907b341168db84c1b0630586db7cbf800aeb194ed832462d45e3b8bea346365"
     end
   end
 
   on_linux do
-    url "https://github.com/docdyhr/batless/releases/download/v0.5.0/batless-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "a94c1de35c223d202ac61af8d06c5cd770a7cb1affb3f3586eed63ce1d4c3d69"
+    url "https://github.com/docdyhr/batless/releases/download/v0.6.0/batless-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "fdf147a267066855af703a44b48aa0c9a2707885453ac98a710772e6b9c59d34"
   end
 
   def install
